474dab6b55e1957597dda0623fa7ae3b9f8dd01e
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-02-01 08:55:48 +0900

    定数と変数の復習をした

--- src/main/kotlin/lesson.kt
+++ src/main/kotlin/lesson.kt
@@ -1,4 +1,18 @@
 fun main() {
+
+    // 定数
     val name = "Kotlin"
+
     println("Hello, $name!")
+    println(name::class)
+
+    // 変数
+    var age = 5
+
+    println("age = $age")
+
+    age = 10
+
+    println("age = $age")
+
 }
