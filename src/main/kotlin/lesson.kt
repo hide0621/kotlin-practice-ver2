a9fd628b4a0d96c5874c9c64e333ad8d711769a9
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-02-01 11:32:39 +0900

    配列の基本形を復習した

--- src/main/kotlin/lesson.kt
+++ src/main/kotlin/lesson.kt
@@ -1,12 +1,33 @@
 fun main() {
 
-    var name:String? = null
+    var array = arrayOf(1, 2, 3, 4, 5)
 
-    if (name != null) {
-        println(name.length)
-    } else {
-        println("name is null")
-        println(name?.length ?: "名前がnullになってます。")
+//    println(array[0])
+
+    /*
+    下５つの処理は全て同じ結果になる
+    downToは降順になる
+    untilは最後の値を含まない
+    */
+
+
+    for (i in array) {
+        println(i)
     }
+
+    for (i in 0..4) {
+        println(array[i])
+    }
+
+    for (i in 0 until 5) {
+        println(array[i])
+    }
+
+    for (i in 4 downTo 0) {
+        println(array[i])
+    }
+
+    // forEach内はラムダ式
+    array.forEach { println(it) }
 
 }
