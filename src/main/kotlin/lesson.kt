261f1c4b48982a8edaf999dd3b27d48ea99a393c
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-02-01 09:51:36 +0900

    Any型の復習をした

--- src/main/kotlin/lesson.kt
+++ src/main/kotlin/lesson.kt
@@ -1,21 +1,11 @@
 fun main() {
 
-    var num = 10
-    var num2: Int = 20
-    var num3 = 30L
+    var any:Any = "Hello"
+    println("$any is ${any::class}")
+    println("$any is ${any::class.simpleName}")
 
-    println(num::class)
-    println(num2::class)
-    println("$num3: ${num3::class}")
-
-    // アンサインド（符号なし）とすることでマイナスの範囲を除外し、byteの範囲を0~255とする
-    var num4: UByte = 255U
-    println("$num4: ${num4::class}")
-
-    var str: String = "Hello"
-    var ch: Char = 'A'
-
-    println("$str: ${str::class}")
-    println("$ch: ${ch::class}")
+    any = 1
+    println("$any is ${any::class}")
+    println("$any is ${any::class.simpleName}")
 
 }
