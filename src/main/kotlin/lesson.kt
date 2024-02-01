29f3a882406c17a9f10450170d0be58603f4e0c1
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-02-01 11:02:28 +0900

    nullの扱われ方について復習した

--- src/main/kotlin/lesson.kt
+++ src/main/kotlin/lesson.kt
@@ -1,23 +1,50 @@
 fun main() {
 
-    var x: Int = 10
+    // kolinはデフォルトでnullを許容しない
+//    var str:String = null
 
-    println("$x = ${x::class.simpleName}")
+    // nullを許容する場合は?（セーフコール演算子）をつける
+    var str2:String? = null
 
-    var y: Float = x.toFloat()
+    //メソッドの実行時にもセーフコール演算子は使える
 
-    println("$y = ${y::class.simpleName}")
+    var name = "hoge"
+    println(name.length) // 4
 
-    var z: String = x.toString()
+    var str3:String? = null
+    println(str3?.length) // null
 
-    println("$z = ${z::class.simpleName}")
+    // nullを返すとまずい場合はエルビス演算子を使う
+    var email:String? = null
+    println(email?.length ?: "") // 空文字
+    println(email?.length ?: "メールアドレスが登録されていません") // メールアドレスが登録されていません
+    println(email?.length ?: 0) // 0
 
-    var dd = 1.6
+    // !!（ヌル安全演算子）を使うとnullを許容しないと宣言できる
+    var num: Int? = 10
 
-    println("$dd = ${dd::class.simpleName}")
+    // null許容型と算術演算子は一緒に使えない
+//    println(num * 10)
 
-    var d: Int = dd.toInt()
+    // 一緒に使うには「スマートキャスト」と!!（ヌル安全演算子）を使う２パターンがある
 
-    println("$d = ${d::class.simpleName}") // 1
+    // スマートキャスト
+    // if文の中でnullでないことが確定している場合に使える(numの型がnull許容型であっても、Int?型をInt型にキャストしてくれる)
+    if (num != null) {
+        println(num * 10)
+    }
+
+    // !!（ヌル安全演算子）
+    // nullでないことを明示的に宣言する
+    println(num!! * 10)
+
+    var num1: Int? = 10
+
+    var num2: Int = num1!!
+    println(num2 * 10)
+
+    // ただし、!!（ヌル安全演算子）はnullの場合に例外を投げるので、nullの可能性がある場合は使わない方が良い
+    var num3: Int? = null
+//    var num4: Int = num3!! // kotlin.KotlinNullPointerException
 
 }
