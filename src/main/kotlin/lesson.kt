c1e3b0c3cb53681275d280a2d5ca7c4589ba0b8f
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-02-01 09:38:09 +0900

    基本的なデータ型の復習をした

--- src/main/kotlin/lesson.kt
+++ src/main/kotlin/lesson.kt
@@ -1,18 +1,21 @@
 fun main() {
 
-    // 定数
-    val name = "Kotlin"
+    var num = 10
+    var num2: Int = 20
+    var num3 = 30L
 
-    println("Hello, $name!")
-    println(name::class)
+    println(num::class)
+    println(num2::class)
+    println("$num3: ${num3::class}")
 
-    // 変数
-    var age = 5
+    // アンサインド（符号なし）とすることでマイナスの範囲を除外し、byteの範囲を0~255とする
+    var num4: UByte = 255U
+    println("$num4: ${num4::class}")
 
-    println("age = $age")
+    var str: String = "Hello"
+    var ch: Char = 'A'
 
-    age = 10
-
-    println("age = $age")
+    println("$str: ${str::class}")
+    println("$ch: ${ch::class}")
 
 }
