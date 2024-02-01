ddc97aee7f6cf1a3b55ad99f650ebb8a24550a16
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-02-01 14:54:58 +0900

    コレクション型のうち、マップ型の復習をした

--- src/main/kotlin/lesson.kt
+++ src/main/kotlin/lesson.kt
@@ -2,19 +2,18 @@ fun main() {
 
     // コレクション型...可変長
     // 例：リスト、セット、マップ
-    // セット...順番の概念がなく（インデックス番号がなく）、重複した要素を持たない
+    // マップ...キーと値のペア, キーは重複不可
 
-    var set = mutableSetOf(10, 20, 30)
-    println(set)
+    var map = mutableMapOf("a" to 1, "b" to 2, "c" to 3)
+    println(map)
 
     // 要素の追加
-    set.add(40)
-    println(set)
+    map["d"] = 4
+    println(map)
 
     // 要素の削除
-    set.remove(30)
-    println(set)
-
+    map.remove("a")
+    println(map)
 
 
 }
