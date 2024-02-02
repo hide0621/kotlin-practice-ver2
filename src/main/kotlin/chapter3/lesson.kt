7972fe2cea4fbbcddd5d9d52f7f57cfb00b8ad84
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-02-02 11:55:05 +0900

    if文とwhen文の復習をした

--- src/main/kotlin/chapter3/lesson.kt
+++ src/main/kotlin/chapter3/lesson.kt
@@ -2,68 +2,63 @@ package chapter3
 
 fun main() {
 
-    // 論理演算子
-    // 否定表現はGo言語のそれと同じ
-    var z = 10
-    println(!(z > 5))
-    println(!true)
-    println(!false)
+    var num = 120
 
-    var ok = true
-    if (ok) {
-        println("OK")
+    // if文の書き方
+    if (num > 100) {
+        println("Greater than 100")
+    } else if (num > 50) {
+        println("Greater than 50")
     } else {
-        println("NG")
+        println("Less than 50")
     }
 
-    if (!ok) {
-        println("NG")
+    // if文の書き方（変数に代入する）
+    var str = if (num > 100) {
+        "Greater than 100"
+    } else if (num > 50) {
+        "Greater than 50"
     } else {
-        println("OK")
+        "Less than 50"
+    }
+    println(str)
+
+    // when文の書き方（Go言語のswitch、case文に似ている）
+    when (num) {
+        100 -> println("Hundred")
+        50 -> println("Fifty")
+        5, 6, 7 -> println("Five or Six or Seven")
+        in 11..20 -> println("Between 11 and 20")
+//        else -> println("Neither")
+        else -> {
+            println("Neither")
+            println("該当する数字がない")
+        }
     }
 
-    // 範囲演算子
-    // 1..5 は 1, 2, 3, 4, 5 という範囲を表す
-    for (i in 1..5) {
-        println(i)
+    when {
+        num > 100 -> println("Greater than 100")
+        num > 50 -> println("Greater than 50")
+        else -> println("Less than 50")
     }
 
-    // 1 until 5 は 1, 2, 3, 4 という範囲を表す
-    for (i in 1 until 5) {
-        println(i)
+    // when文の書き方（変数に代入する）
+    var str2 = when {
+        num > 100 -> "Greater than 100"
+        num > 50 -> "Greater than 50"
+        else -> "Less than 50"
     }
+    println(str2)
 
-    // 5 downTo 1 は 5, 4, 3, 2, 1 という範囲を表す
-    for (i in 5 downTo 1) {
-        println(i)
+    // when文の書き方（型チェック）
+    var num2: Any = "10"
+
+    when (num2) {
+        is Int -> println("Int")
+        is Double -> println("Double")
+        is String -> println("String")
+        else -> println("該当する型がない")
     }
-
-    // 1..5 step 2 は 1, 3, 5 という範囲を表す
-    for (i in 1..5 step 2) {
-        println(i)
-    }
-
-    // 5 downTo 1 step 2 は 5, 3, 1 という範囲を表す
-    for (i in 5 downTo 1 step 2) {
-        println(i)
-    }
-
-    // in演算子：対象が範囲内にあるかどうかを判定する
-    var x = 3
-    if (x in 1..5) {
-        println("OK")
-    } else {
-        println("NG")
-    }
-
-    var n1 = 10
-    var n2 = 20
-    var array = arrayOf(n1, n2)
-    println(15 in n1..n2) // 10 <= 15 <= 20 なのでtrue
-    println(15 !in n1..n2) // 10 <= 15 <= 20 なのでfalse
-    println(15 in array) // 10, 20 なのでfalse
-    println(15 !in array) // 10, 20 なのでtrue
-    println("B" in "A".."C") // "A" <= "B" <= "C" なのでtrue
 
 }
 
