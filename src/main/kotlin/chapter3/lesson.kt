1ee2cca1e638c51ecccc7cf9e673d89fcd720667
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-02-02 15:25:06 +0900

    コレクション型のオブジェクトを使ったfor文の復習をした

--- src/main/kotlin/chapter3/lesson.kt
+++ src/main/kotlin/chapter3/lesson.kt
@@ -2,28 +2,51 @@ package chapter3
 
 fun main() {
 
-    for (i in 1..10) {
-        for (j in 1..10) {
-            print("${i * j} ")
-        }
+    // 配列のパターン
+    // リストもこれと同じ
+
+    var array = arrayOf(1, 2, 3, 4, 5)
+    for (i in array) {
+        println(i)
     }
 
-    // 見づらいので修正
-    for (i in 1..10) {
-        for (j in 1..10) {
-            print("${i * j} ")
-        }
-        println()
+    // インデックスも表示
+    for ((index, value) in array.withIndex()) {
+        println("the element at $index is $value")
     }
 
-    // format関数を使って見やすくする
-    for (i in 1..10) {
-        for (j in 1..10) {
-            print("%2d ".format(i * j))
-        }
-        println()
+    // インデックスだけindexに渡す
+    for (index in array.indices) {
+        println("the element at $index is ${array[index]}")
     }
 
+    // セットのパターン
+    var set = setOf(1, 2, 3, 4, 5)
+    for (i in set) {
+        println(i)
+    }
+
+    // マップのパターン
+    var map = mapOf(1 to "one", 2 to "two", 3 to "three")
+    for (kv in map) {
+        println(kv) // 1=one, 2=two, 3=three
+    }
+
+
+    // キーと値を同時に
+    for ((key, value) in map) {
+        println("the element at $key is $value")
+    }
+
+    // mapのキーだけkeyに渡す
+    for (key in map.keys) {
+        println("the element at $key is ${map[key]}")
+    }
+
+    // mapの値だけ
+    for (value in map.values) {
+        println("the element is $value")
+    }
 
 }
 
