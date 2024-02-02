d257feaf2a6b90aec303f5715efc4509b8cd1c49
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-02-02 14:48:48 +0900

    while文の復習をした

--- src/main/kotlin/chapter3/lesson.kt
+++ src/main/kotlin/chapter3/lesson.kt
@@ -2,63 +2,27 @@ package chapter3
 
 fun main() {
 
-    var num = 120
-
-    // if文の書き方
-    if (num > 100) {
-        println("Greater than 100")
-    } else if (num > 50) {
-        println("Greater than 50")
-    } else {
-        println("Less than 50")
+    // while : 前判定
+    var i = 0
+    while (i < 10) {
+        println(i)
+        i++
     }
 
-    // if文の書き方（変数に代入する）
-    var str = if (num > 100) {
-        "Greater than 100"
-    } else if (num > 50) {
-        "Greater than 50"
-    } else {
-        "Less than 50"
-    }
-    println(str)
+    // do-while : 後判定
+    var j = 0
+    do {
+        println(j)
+        j++
+    } while (j < 10)
 
-    // when文の書き方（Go言語のswitch、case文に似ている）
-    when (num) {
-        100 -> println("Hundred")
-        50 -> println("Fifty")
-        5, 6, 7 -> println("Five or Six or Seven")
-        in 11..20 -> println("Between 11 and 20")
-//        else -> println("Neither")
-        else -> {
-            println("Neither")
-            println("該当する数字がない")
-        }
-    }
-
-    when {
-        num > 100 -> println("Greater than 100")
-        num > 50 -> println("Greater than 50")
-        else -> println("Less than 50")
-    }
-
-    // when文の書き方（変数に代入する）
-    var str2 = when {
-        num > 100 -> "Greater than 100"
-        num > 50 -> "Greater than 50"
-        else -> "Less than 50"
-    }
-    println(str2)
-
-    // when文の書き方（型チェック）
-    var num2: Any = "10"
-
-    when (num2) {
-        is Int -> println("Int")
-        is Double -> println("Double")
-        is String -> println("String")
-        else -> println("該当する型がない")
-    }
+    // 故にこうなる
+    // こう言うことになるので、使わない方がいいかも...
+    var k = 10
+    do {
+        println(k) // 10 が出力される
+        k++
+    } while (k < 10)
 
 }
 
